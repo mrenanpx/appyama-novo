d538abe405d788434cd32ac3a178f661501b0346
mrenanpx.art <mrenanpx@gmail.com>
2026-08-27 18:07:59 -0300

    Atualizar o vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/appyama-novo/', // <-- Adicione esta linha (substitua pelo nome exato do seu repositório se for diferente)
+  base: '/appyama-novo/'
 })
