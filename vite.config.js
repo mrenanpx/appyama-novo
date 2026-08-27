34da73d8aa636a07cd949d39eb7f5982f4264a4b
Renan Pagliai <mrenanpx@gmail.com>
2026-08-27 17:02:08 -0300

    Ajuste base url para o GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/appyama-novo/', // <-- Adicione esta linha (substitua pelo nome exato do seu repositório se for diferente)
 })
